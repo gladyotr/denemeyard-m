8ea06713c7699cac3f20a80e3f706940dc637eb0
gladyotr <56080136+gladyotr@users.noreply.github.com>
2021-10-28 21:06:07 +0300

    Add files via upload

---
+++ yardım.py
@@ -0,0 +1,201 @@
+import time
+import os, sys
+import configparser
+os.system('clear')
+time.sleep(2)
+print("""❰█          ❱""")
+time.sleep(0.5)
+os.system('clear')
+print("""❰██         ❱""")
+time.sleep(0.5)
+os.system('clear')
+print("""❰███        ❱""")
+time.sleep(0.5)
+os.system('clear')
+print("""❰████       ❱""")
+time.sleep(0.5)
+os.system('clear')
+print("""❰█████      ❱""")
+time.sleep(0.5)
+os.system('clear')
+print("""❰██████     ❱""")
+time.sleep(0.5)
+os.system('clear')
+print("""❰███████    ❱""")
+time.sleep(0.5)
+os.system('clear')
+print("""❰████████   ❱""")
+time.sleep(0.5)
+os.system('clear')
+print("""❰█████████  ❱""")
+time.sleep(0.5)
+os.system('clear')
+print("""❰██████████ ❱""")
+time.sleep(0.5)
+os.system('clear')
+print("""❰███████████❱""")
+time.sleep(3)
+os.system('clear')
+print("""
+################################""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#
+#[-] Hesap eklemek için        #""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#
+#[-] Hesap eklemek için        #
+#[+] python3 setup.py          #""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#
+#[-] Hesap eklemek için        #
+#[+] python3 setup.py          #
+#------------------------------#""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#
+#[-] Hesap eklemek için        #
+#[+] python3 setup.py          #
+#------------------------------#
+#[-] Sisteme üye çekmek için   #""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#
+#[-] Hesap eklemek için        #
+#[+] python3 setup.py          #
+#------------------------------#
+#[-] Sisteme üye çekmek için   #
+#[+] python3 scraper.py        #""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#
+#[-] Hesap eklemek için        #
+#[+] python3 setup.py          #
+#------------------------------#
+#[-] Sisteme üye çekmek için   #
+#[+] python3 scraper.py        #
+#------------------------------#""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#
+#[-] Hesap eklemek için        #
+#[+] python3 setup.py          #
+#------------------------------#
+#[-] Sisteme üye çekmek için   #
+#[+] python3 scraper.py        #
+#------------------------------#
+#[-] Üyeleri gruba eklemek için#""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#
+#[-] Hesap eklemek için        #
+#[+] python3 setup.py          #
+#------------------------------#
+#[-] Sisteme üye çekmek için   #
+#[+] python3 scraper.py        #
+#------------------------------#
+#[-] Üyeleri gruba eklemek için#
+#[+] python3 adder.py          #""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#
+#[-] Hesap eklemek için        #
+#[+] python3 setup.py          #
+#------------------------------#
+#[-] Sisteme üye çekmek için   #
+#[+] python3 scraper.py        #
+#------------------------------#
+#[-] Üyeleri gruba eklemek için#
+#[+] python3 adder.py          #
+#------------------------------#""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#
+#[-] Hesap eklemek için        #
+#[+] python3 setup.py          #
+#------------------------------#
+#[-] Sisteme üye çekmek için   #
+#[+] python3 scraper.py        #
+#------------------------------#
+#[-] Üyeleri gruba eklemek için#
+#[+] python3 adder.py          #
+#------------------------------#
+#     Yapımcı @gladbey         #""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#
+#[-] Hesap eklemek için        #
+#[+] python3 setup.py          #
+#------------------------------#
+#[-] Sisteme üye çekmek için   #
+#[+] python3 scraper.py        #
+#------------------------------#
+#[-] Üyeleri gruba eklemek için#
+#[+] python3 adder.py          #
+#------------------------------#
+#     Yapımcı @gladbey         #
+#     Yapım Grubu @folunted    #""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#
+#[-] Hesap eklemek için        #
+#[+] python3 setup.py          #
+#------------------------------#
+#[-] Sisteme üye çekmek için   #
+#[+] python3 scraper.py        #
+#------------------------------#
+#[-] Üyeleri gruba eklemek için#
+#[+] python3 adder.py          #
+#------------------------------#
+#     Yapımcı @gladbey         #
+#     Yapım Grubu @folunted    #
+################################""")
+time.sleep(0.3)
+os.system('clear')
+print("""
+################################
+#------------------------------#
+#[-] Hesap eklemek için        #
+#[+] python3 setup.py          #
+#------------------------------#
+#[-] Sisteme üye çekmek için   #
+#[+] python3 scraper.py        #
+#------------------------------#
+#[-] Üyeleri gruba eklemek için#
+#[+] python3 adder.py          #
+#------------------------------#
+#     Yapımcı @gladbey         #
+#     Yapım Grubu @folunted    #
+################################
+        """)
